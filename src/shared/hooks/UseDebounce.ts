80bd95802beb0ece7b6512b63cea0a7d8f2fb118
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-10-22 09:59:09 -0300

    Update UseDebounce.ts

--- src/shared/hooks/UseDebounce.ts
+++ src/shared/hooks/UseDebounce.ts
@@ -1,20 +1,23 @@
 import { useCallback, useRef } from "react";
 
 export const useDebounce = (delay = 500, notDelayInFirstTime = true) => {
-    const debouncing = useRef<ReturnType<typeof setTimeout> | null>(null);
-    const isFirstTime = useRef(notDelayInFirstTime);
+  const debouncing = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const isFirstTime = useRef(notDelayInFirstTime);
 
-    const debounce = useCallback((func: () => void) => {
-        if (isFirstTime.current) {
-            isFirstTime.current = false;
-            func();
-        } else {
-            if (debouncing.current) {
-                clearTimeout(debouncing.current);
-            }
-            debouncing.current = setTimeout(() => func(), delay);
+  const debounce = useCallback(
+    (func: () => void) => {
+      if (isFirstTime.current) {
+        isFirstTime.current = false;
+        func();
+      } else {
+        if (debouncing.current) {
+          clearTimeout(debouncing.current);
         }
-    }, [delay]);
+        debouncing.current = setTimeout(() => func(), delay);
+      }
+    },
+    [delay]
+  );
 
-    return { debounce };
+  return { debounce };
 };
